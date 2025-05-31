1f9b3b1393546bc08d5d5ed85065801660899f4c
jabirstain3 <mrjabir229@gmail.com>
2025-05-31 06:57:09 +0600

    Toast provider and auth context and firebase configaration

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import router from './router/Router.jsx'
 import { RouterProvider } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContextProvider.jsx'
+import ToastProvider from './toastProvider/ToastProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <RouterProvider router={router} />
+    <ToastProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </ToastProvider>
   </StrictMode>,
 )
